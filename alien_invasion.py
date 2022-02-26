1ae0b5ddd667794227cf55d1f704ffda938556da
Ferliness <reason.life@yandex.ru>
2022-02-26 18:05:27 +0300

    Feature: the player's ship is drawn on the screen,#TODO size of ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,8 @@
 import sys
-
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     pygame.init()
@@ -11,6 +11,9 @@ def run_game():
     game_screen = pygame.display.set_mode(
         (ai_settings.screen_wigth,ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+    
+    player_ship = Ship(game_screen)
+    #TODO size of ship
     
     # Starting the main loop of game.
     while True:
@@ -21,6 +24,8 @@ def run_game():
         
         # The screen is redrawn on each iteration
         game_screen.fill(ai_settings.background_color)
+        
+        player_ship.blitme() 
         
         # Display the last screen drawn.        
         pygame.display.flip()
