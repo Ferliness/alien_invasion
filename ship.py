a40fd1571d1ea1bda8f3877fc0aab59848fccdf5
Ferliness <reason.life@yandex.ru>
2022-02-26 17:48:20 +0300

    Fix: replace images/ship.bmp with images/ship.png

--- ship.py
+++ ship.py
@@ -5,7 +5,7 @@ class Ship:
     def __init__(self, game_screen) -> None:
         self.game_screen = game_screen
         
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship.png')
         self.rect = self.image.get_rect()
         self.screen_rect = game_screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
