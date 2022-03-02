5210631311933e23310b71551bec5639c53a333a
Ferliness <reason.life@yandex.ru>
2022-03-02 22:18:35 +0300

    Feature: background color shifted towards light blue

--- settings.py
+++ settings.py
@@ -6,4 +6,4 @@ class Settings:
     def __init__(self) -> None:
         self.screen_wigth = 1200
         self.screen_height = 800
-        self.background_color = (230, 230, 230)
+        self.background_color = (150, 200, 255)
